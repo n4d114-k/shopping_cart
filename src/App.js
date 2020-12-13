6ac8d94495f77ca872042afdd43ce369673b58f8
n4d114-k <n4d114.k@gmail.com>
2020-12-13 17:16:27 +0200

    add nav styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>Header</h1>
+    <div className="app-container">
+      <header>
+        <nav>
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1>GROCERY STORE</h1>
+          <ul>
+            <li><a href="#">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">cart</a></li>
+          </ul>
+        </nav>
+      </header>
+      <br />
       <p>Text sample</p>
     </div>
   );
